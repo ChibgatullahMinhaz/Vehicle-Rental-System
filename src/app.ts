8b53a0eb7e97612f8e2249f0a58b3532a6114163
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-12-06 00:46:17 +0600

    not found

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import { Request, Response, urlencoded } from "express";
 import express from "express";
+import router from "./routes/routes";
+import intiDB from "./config/db";
+import notFound from "./middleware/notFound";
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
+
+intiDB();
+app.use("/api/v1", router);
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+    res.send("Hello World!");
 });
 
+app.use(notFound)
 export default app;
--- src/config/db.ts
+++ src/config/db.ts
@@ -2,8 +2,24 @@ import { Pool } from "pg";
 import config from "./config";
 
 export const pool = new Pool({
-    connectionString: `${config.connection_str}`
-})
+  connectionString: `${config.connection_str}`,
+});
 
+const intiDB = async () => {
+  console.log("db in initialized");
+  await pool.query(
+    `CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE CHECK (email = LOWER(email)),
+    password TEXT NOT NULL CHECK (char_length(password) >= 6),
+    phone TEXT NOT NULL,
+    role TEXT NOT NULL CHECK (role IN ('admin', 'customer')),
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+    )`
+  );
+  console.log("user table is ready");
+};
 
-const intiDB = () => {};
+export default intiDB;
