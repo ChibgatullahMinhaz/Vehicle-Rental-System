27fa1df26346c25e035a28d52149e79fca3675ff
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-12-06 01:20:42 +0600

    makes sql table

--- src/config/db.ts
+++ src/config/db.ts
@@ -20,6 +20,34 @@ const intiDB = async () => {
     )`
   );
   console.log("user table is ready");
+
+  await pool.query(`
+    CREATE TABLE IS NOT EXISTS Vehicles (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    vehicle_name VARCHAR(200) NOT NUL ,
+    type TEXT NOT NULL CHECK (type IN ('car', 'bike', 'van', 'SUV')),
+    registration_number VARCHAR(50) NOT NULL UNIQUE ,
+    daily_rent_price NUMERIC(10,2) NOT NULL CHECK (daily_rent_price > 0),
+    availability_status TEXT NOT NULL CHECK (availability_status IN ('available', 'booked'))  DEFAULT 'available',
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+    )
+    `);
+
+  await pool.query(`
+      CREATE TABLE IS NOT EXISTS Bookings(
+      id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+      customer_id UUID REFERENCES users(id) ON DELETE CASCADE,
+      vehicle_id UUID REFERENCES Vehicles(id) ON DELETE CASCADE,
+      rent_start_date DATE NOT NULL ,
+      rent_end_date DATE NOT NULL CHECK (rent_end_date>rent_start_date),
+      total_price NUMERIC(10,2) NOT NULL CHECK (total_price > 0),
+      status TEXT NOT NULL CHECK (status IN ('active', 'cancelled', 'returned'))
+        DEFAULT 'active',
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+      )
+      `);
 };
 
 export default intiDB;
